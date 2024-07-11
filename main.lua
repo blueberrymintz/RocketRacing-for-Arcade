160fe16567f65383f509dff042ff74fb5d8eafc7
lily <beelzebufo@icloud.com>
2024-07-10 17:27:49 -0700

    imported the files for car and made them a sprite

--- main.lua
+++ main.lua
@@ -8,7 +8,7 @@ Car.polardirection = 0
 Car.hp = 0
 Car.traction = 0
 Car.acceleration = 0
-Car.sprite = love.graphics.newImage('sprites/High_pix_car.png')
+Car.sprite = love.graphics.newImage('sprites/no_background_1st_pixel_car.png')
 end
 
 function love.update(dt)
@@ -19,4 +19,5 @@ end
 
 function love.draw()
     love.graphics.draw(Car.sprite, Car.x, Car.y)
+    love.graphics.setBackgroundColor(300,300,300)
 end
